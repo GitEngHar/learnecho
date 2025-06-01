db7c3994a7e6576ba6ed2f083b61e0c89ee4da16
GitEngHar <harukii1999@gmail.com>
2025-06-01 15:25:39 +0900

    create

---
+++ README.md
@@ -0,0 +1,20 @@
+# Let's try Echo
+
+```shell
+go run server.go
+
+   ____    __
+  / __/___/ /  ___
+ / _// __/ _ \/ _ \
+/___/\__/_//_/\___/ v4.13.4
+High performance, minimalist Go web framework
+https://echo.labstack.com
+____________________________________O/_______
+                                    O\
+⇨ http server started on [::]:1323
+```
+
+## Run Example
+
+After run server , Redirect this [page](http://localhost:1323/).
+
---
+++ server.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"github.com/labstack/echo/v4"
+	"net/http"
+)
+
+func getUser(c echo.Context) error {
+	id := c.Param("id")
+	return c.String(http.StatusOK, id)
+}
+
+// queryParam
+func show(c echo.Context) error {
+	team := c.QueryParam("team")
+	member := c.QueryParam("member")
+	return c.String(http.StatusOK, "team:"+team+", member:"+member)
+}
+
+// form
+func save(c echo.Context) error {
+	name := c.FormValue("name")
+	email := c.FormValue("email")
+	return c.String(http.StatusOK, "name: "+name+", email: "+email)
+}
+func main() {
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello World")
+	})
+	// http://localhost:1323/users/har
+	// The return string is 'har'
+	e.GET("/users/:id", getUser)
+
+	// http://localhost:1323/show?team=dev&member=har
+	// The return string is 'team:dev, member:har'
+	e.GET("/show", show)
+
+	// curl -d "name=git eng" -d "email=hoge@gmail.com" http://localhost:1323/save
+	// namegit eng, email:hoge@gmail.com
+	e.POST("/save", save)
+
+	e.Logger.Fatal(e.Start(":1323"))
+}
