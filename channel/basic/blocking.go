cda97bc61a3a3becd89652a181a77eedee3427da
GitEngHar <harukii1999@gmail.com>
2025-06-21 10:11:23 +0900

    add blocking

--- channel/basic/blocking.go
+++ channel/basic/blocking.go
@@ -7,18 +7,18 @@ import (
 
 func main() {
 	// cap 2
-	ch := make(chan int, 5)
-
+	ch := make(chan int, 2) // capが送信されるデータよりも少ないため、空きができるまで待ちとなる
 	go func() {
 		for i := 0; i < 5; i++ {
 			fmt.Println("send to channel")
 			ch <- i
 		}
-		close(ch) //これがないとチャネルからの値を待ち続ける。永遠にブロッキングとなる。
+		defer close(ch) //これがないとチャネルからの値を待ち続ける。永遠にブロッキングとなる。
 	}()
-
 	time.Sleep(1 * time.Second)
-	for msg := range ch {
+	fmt.Println("Sleep 1s")
+	for msg := range ch { //チャネルが閉じるまでデータ取得する
 		fmt.Println("Received %d", msg)
 	}
+
 }
